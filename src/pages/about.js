1d7f8584d51cd382a5f73cc31f24b2701da37cdb
Julius Herbert <juliush531@gmail.com>
2021-02-26 02:54:36 +0100

    add more pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/header';
+
+export default function About() {
+  return (
+    <div style={{ color: 'teal' }}>
+      <Header headerText="Hello Gatsby." />
+      <Header headerText="It is pretty cool" />
+      <p>Such now. Very React.</p>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
-import React from "react"
+import React from 'react';
+import Header from '../components/header';
+import { Link } from 'gatsby';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: 'purple' }}>
+      <Link to="/contact/">contact</Link>
+      <Header headerText="Hello gatsby!" />
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/200x200" alt="" />
+    </div>
+  );
 }
